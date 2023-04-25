d737b8eb06f36ccecf57bbacbcc7ec45bb27698e
Lukasz Grot <lukasgrot98@gmail.com>
2023-04-25 21:46:51 +0200

    next file commited and pushed using pycharm

--- head first.py
+++ head first.py
@@ -11,4 +11,34 @@ count=0
 while count <len(movies):
     print(movies[count])
     count=count +1
-    print(count)
+
+names=['Lukasz', 'Magdalena', ['John', 'Noemi', ['Beatrix', 'Whitney', [1,2,3]]]]
+isinstance(names, list)
+licz_names=len(names)
+isinstance(licz_names, list)
+
+print(names[1])
+
+for each_item in names:
+    if isinstance(each_item, list):
+        for nested_item in each_item:
+            if isinstance(nested_item, list):
+                for inner_list in nested_item:
+                    if isinstance(inner_list, list):
+                        for numbers in inner_list:
+                            print(numbers)
+                    else:
+                        print(inner_list)
+            else:
+                print(nested_item)
+    else:
+        print(each_item)
+the_list=[4,5,6]
+
+def print_lol(the_list):
+    for new in the_list:
+        if isinstance(new, list):
+            print_lol(new)
+        else:
+            print(new)
+print_lol(names)
